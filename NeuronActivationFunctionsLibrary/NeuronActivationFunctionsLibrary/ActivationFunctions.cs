58a60b97cdecb57b889868ab35510e840aaffa6b
mikechuhmar <miha1797@gmail.com>
2018-10-09 01:17:41 +0300

    Create project

---
+++ NeuronActivationFunctionsLibrary/NeuronActivationFunctionsLibrary/ActivationFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuronActivationFunctionsLibrary
+{
+    public class ActivationFunctions
+    {
+        public static double Binary(double signal, double threshold)
+        {
+            return signal > threshold ? 1 : 0;
+        }
+        public static double Binary(double signal)
+        {
+            return Binary(signal, 0);
+        }
+        public static double Bipolar(double signal, double threshold)
+        {
+            return signal > threshold ? 1 : -1;
+        }
+        public static double Bipolar(double signal)
+        {
+            return Bipolar(signal, 0);
+        }
+
+    }
+}
